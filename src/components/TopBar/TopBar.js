7a60f17331ea94103c7f5426fc37f237fb0d171d
Eric <ericd3770@gmail.com>
2024-07-24 14:39:40 -0400

    updated topbar

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -9,17 +9,17 @@ import DateRangeButtons from './DateRangeButtons';
 const TopBar = () => {
   return (
     <div className="absolute top-0 left-0 w-full h-[106px] flex items-center justify-end pr-[40px] bg-color-white-100 text-black1 text-xl">
-      {/* <div className="flex items-center justify-between w-full max-w-[1631px] p-5">
-        <div className="flex items-center"> */}
+      <div className="flex items-center justify-between w-full max-w-[1531px] p-5">
+        <div className="flex items-center">
           <Logo />
-        {/* </div> */}
+        </div>
         <div className="flex items-center gap-12 text-white pr-[80px]">
           <ThemeToggle />
           <AddEvaluationsButton />
           <NotificationIcon />
           <UserProfile />
         </div>
-      {/* </div> */}
+      </div>
       <div className="absolute top-[28px] left-0 w-full flex items-center justify-end px-[75px] text-sm text-black">
         {/* <DateRangeButtons /> */}
       </div>
