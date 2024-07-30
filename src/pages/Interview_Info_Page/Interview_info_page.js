77dd158179f1bde7278795f49156e883bb2788de
Eric <ericd3770@gmail.com>
2024-07-30 14:43:51 -0400

    added interview display

--- src/pages/Interview_Info_Page/Interview_info_page.js
+++ src/pages/Interview_Info_Page/Interview_info_page.js
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
 import { useTheme } from "../../themeContext"; // Import the custom hook
 import TopBar from "../../components/TopBar/TopBar";
 import SideMenu from "../../components/SideMenu/SideMenu";
-import EvaluationContainer from "./EvaluationContainer";
-import FloatingButtons from "./FloatingButtons";
-import Header from "./Header";
+import React, { useState, useEffect } from "react";
+
 import { useParams } from "react-router-dom";
 import Evaulations3 from "./Evaulations3";
+import axios from "axios";
 
 const Interview_info_page = () => {
   const { theme } = useTheme(); // Get the current theme
   const { id, interviewid } = useParams(); // Get the URL parameters
+
+
+
+  const [interviewDetails, setInterviewDetails] = useState({
+    interview_date: '',
+    interviewer_name: '',
+    interview_type: ''
+  });
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/interviews/${id}/${interviewid}`)
+      .then(response => {
+        setInterviewDetails(response.data);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the interview details!", error);
+      });
+  }, [id, interviewid]);
 
   return (
     <div
@@ -23,7 +40,8 @@ const Interview_info_page = () => {
 
       <div className="absolute top-[169px] left-[276px] w-[1175px] flex flex-row items-start justify-start py-0 px-[75px] box-border max-w-full z-[2] text-13xl text-black font-poppins">
         <div className="h-[22px] relative font-semibold inline-block mq450:text-lgi mq850:text-7xl">
-          Travis Durand Interview
+       Travis Durand
+
         </div>
       </div>
 
@@ -38,7 +56,7 @@ const Interview_info_page = () => {
       </div>
 
       <div className="absolute top-[507px] left-[334px] rounded-sm-5 bg-color-white-100 w-[1494px] flex flex-row items-start justify-start py-2 pr-[75px] pl-20 box-border max-w-full text-gray">
-        <div className="flex-1 relative leading-[38px] font-semibold inline-block mix-blend-darken max-w-full mq450:text-xl mq450:leading-[30px]">{`Travis Durand `}</div>
+        <div className="flex-1 relative leading-[38px] font-semibold inline-block mix-blend-darken max-w-full mq450:text-xl mq450:leading-[30px]"> {interviewDetails.interviewer_name} Interview</div>
       </div>
 
       <div className="absolute top-[604px] left-[338px] text-3xl-1 leading-[34px] inline-block w-[545.8px] mq450:text-lg mq450:leading-[27px]">
@@ -47,7 +65,8 @@ const Interview_info_page = () => {
 
       <div className="absolute top-[653px] left-[334px] rounded-sm-5 bg-color-white-100 w-[1494px] flex flex-row items-start justify-start py-2 pr-[75px] pl-20 box-border max-w-full text-gray">
         <div className="flex-1 relative leading-[38px] font-semibold inline-block mix-blend-darken max-w-full mq450:text-xl mq450:leading-[30px]">
-          July, 15th, 2023
+        {interviewDetails.interview_date}
+
         </div>
       </div>
 
@@ -57,7 +76,8 @@ const Interview_info_page = () => {
 
       <div className="absolute top-[799px] left-[332px] rounded-sm-5 bg-color-white-100 w-[1494px] flex flex-row items-start justify-start py-2 pr-[75px] pl-20 box-border max-w-full text-gray">
         <div className="flex-1 relative leading-[38px] font-semibold inline-block mix-blend-darken max-w-full mq450:text-xl mq450:leading-[30px]">
-          In Person
+        {interviewDetails.interview_type}
+
         </div>
       </div>
     </div>
